14df6433d9639a17eb23546ede62b59cc6104bfd
Anthony Quinn <58011993+AnthonyQ98@users.noreply.github.com>
2021-01-11 12:25:01 +0000

    main.py updated
    Replaced multiple lists with one nested list + added a checkout total counter.

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ def username():
             print("Incorrect Username!")
             continue
 
+
 def password():
-    passwordAttempts = 0
+    passwordattempts = 0
     while True:
         print("Enter your password: ")
         password = input()
@@ -22,23 +23,46 @@ def password():
             print("Successful login!")
             break
         else:
-            passwordAttempts += 1
-            passwordTry = 5 - passwordAttempts
-            print("Password incorret, you have " + str(passwordTry) + " attempts left.")
-            if passwordAttempts == 5:
+            passwordattempts += 1
+            passwordtry = 5 - passwordattempts
+            print("Password incorrect, you have " + str(passwordtry) + " attempts left.")
+            if passwordattempts == 5:
                 print("You have ran out of attempts, please try again in five minutes.")
             else:
                 continue
 
-#username()
-#password()
+# username()
+# password()
+
 
 mainOrder = ['Big Mac Meal', 'McChicken Sandwich Meal', 'Nuggets']
 sideOrder = ['Fries', 'Fruit bag', 'Curly fries']
-desert = ['McFlurry', ]
+desert = ['McFlurry', 'Cookie', 'McMuffin', 'Donut']
+
+menu_menu = [['Big Mac Meal', 12.50], ['McChicken Sandwich Meal', 10.50], ['Nuggets', 7.50],
+             ['Fries', 3.30], ['Fruit bag', 1.50], ['Curly fries', 4.50],
+             ['McFlurry', 3.50], ['Cookie', 2.20], ['McMuffin', 2.00], ['Donut', 2.50]]
+
 
 def drivethru():
-    print("*" * 50)
+    menu = []
+    total = 0
+    print("*" * 50, "\n Main: ", mainOrder, "\n Side: ", sideOrder, "\n Desert: ", desert)
+    while True:
+        print("Please choose what you'd like or press 2 to checkout: ")
+        pick = input()
+        for sublist in menu_menu:
+            if pick == sublist[0]:
+                total += sublist[1]
+                menu.append(pick)
+                print(str(pick) + " added to your order.")
+            elif pick == '2':
+                print("Your order: \n", ", ".join(menu))
+                print("Your total is: €" + str(round(total, 2)))
+                return total, menu
+
 
 drivethru()
+
+print("Hello!")
 
